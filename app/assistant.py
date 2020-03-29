964d26b89030c44dd91d673c2baa76aa33311667
manchenkoff <artyom@manchenkoff.me>
2020-03-29 12:11:34 +0300

    initial commit

---
+++ app/assistant.py
@@ -0,0 +1,29 @@
+#
+# Пример приложения голосового ассистента
+#
+
+
+def listen():
+    pass
+
+
+def say(text):
+    pass
+
+
+def handle_command(command):
+    pass
+
+
+def stop():
+    pass
+
+
+def start():
+    pass
+
+
+try:
+    start()
+except KeyboardInterrupt:
+    stop()
